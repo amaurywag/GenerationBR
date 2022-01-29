f7b70949a6bcf1d695bd5fded37eb97d8b486c5f
Amaury <86973979+amaurywag@users.noreply.github.com>
2022-01-29 10:37:48 -0300

    Update exercicio_matrizes_1.java

--- eclipse/vetores_matrizes/exercicio_matrizes_1.java
+++ eclipse/vetores_matrizes/exercicio_matrizes_1.java
@@ -1,9 +1,31 @@
 package vetores;
 
-public class exercicio_matrizes_1 {
+import java.util.Scanner;
+
+public class exercicio_matrizes_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Leia uma matriz 3 x 3, conte e escreva quantos valores maiores que 10 ela possui
+
+		int[][] mat = new int[3][3];
+		int l, c, cont=0, con=0;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		for(l=0;l<3;l++) {
+			for(c=0;c<3;c++) {
+				con+=1;
+				System.out.print("Digite o "+con+"º número: ");
+				mat[l][c] = leia.nextInt();
+				int x = 0;
+					x+=mat[l][c];
+				if(x>9) {
+					cont +=1;
+				}
+			}
+		}
+		System.out.println("\n\t\t\tResultado:");
+		System.out.println("\nTiveram "+cont+" números acima de 10");
 
 	}
 
